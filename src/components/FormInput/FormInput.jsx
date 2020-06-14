3a674f08f0c055e92b6e8783059361d7d9707d57
Harkunwar Singh Kochar <geek@harkunwar.com>
2020-06-14 15:35:22 -0700

    small changes

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { StyledInput, StyledLabel, StyledContainer } from './FormInputStyles'
+import { StyledInput, StyledLabel, StyledContainer } from './FormInput.styles'
 
 const FormInput = ({ label, ...otherProps }) => (
   <StyledContainer>
